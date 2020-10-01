5c009489aacb3240a66d7ec991c86ace6cc55db0
Rafael Freitas Quixabeira - RFE <rafael.freitas@iteris.com.br>
2020-10-01 15:44:01 -0300

    criando contador nas libs

--- IterisDynamicLibrary/DynamicLibrary.swift
+++ IterisDynamicLibrary/DynamicLibrary.swift
@@ -1,7 +1,10 @@
 public class DynamicLibrary {
+    public static var counter: Int = 0
+
     public init() {}
 
     public func hello() -> String {
-        return "Hello From Dynamic Library"
+        DynamicLibrary.counter += 1
+        return "Hello From Dynamic Library: \(DynamicLibrary.counter)"
     }
 }
--- IterisStaticLibrary/StaticLibrary.swift
+++ IterisStaticLibrary/StaticLibrary.swift
@@ -1,7 +1,10 @@
 public class StaticLibrary {
+    public static var counter: Int = 0
+
     public init() {}
 
     public func hello() -> String {
-        return "Hello From Static Library"
+        StaticLibrary.counter += 1
+        return "Hello From Static Library: \(StaticLibrary.counter)"
     }
 }
